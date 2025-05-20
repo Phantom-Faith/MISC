cfeb864449c3e6fab94a8583aa2c1b477f88ac7b
phantomFaith <142546561+phantomFaith@users.noreply.github.com>
2025-05-20 10:59:37 +0200

    Update install_agent.sh

--- install_agent.sh
+++ install_agent.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 set -e
@@ -13,14 +12,28 @@ then
     sh get-docker.sh
 fi
 
+echo "Installing restic..."
+curl -L https://github.com/restic/restic/releases/latest/download/restic_$(uname -s | tr '[:upper:]' '[:lower:]')_amd64.bz2 -o restic.bz2
+bunzip2 -f restic.bz2
+chmod +x restic
+mv -f restic /usr/local/bin/restic
+
 echo "Pulling agent image..."
 docker pull phantomfaith/re-agent:latest
+
+# Check if container is running or exists
+if docker ps -a --format '{{.Names}}' | grep -Eq "^re-agent$"; then
+    echo "Removing existing re-agent container..."
+    docker stop re-agent || true
+    docker rm re-agent || true
+fi
 
 echo "Running agent container..."
 docker run -d \
   --name re-agent \
   --restart unless-stopped \
   -v /:/host \
+  -v /usr/local/bin/restic:/usr/bin/restic:ro \
   -p 8081:8080 \
   phantomfaith/re-agent:latest
 
